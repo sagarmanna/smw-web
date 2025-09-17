72e04387714a5455da834e32d3e6ddbaba77e9f7
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-09-17 14:17:57 +0530

    Next cinfig change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  basePath: "/admin/v2",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dev2.studiomanagerweb.com",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+// Hello
