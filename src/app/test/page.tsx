f035a4e0d0453212df387021cb761a083e40187a
manic56 <manikandan@pearlthoughts.com>
2025-09-17 17:07:06 +0530

    Add Test page component to display environment variables

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+const Test = () => {
+    return (
+        <>
+        <div> Test Page </div>
+        <pre>
+        NEXT_PUBLIC_ENV: {process.env.NEXT_PUBLIC_ENV}
+        <br />
+        NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}
+        <br />
+        NEXT_PUBLIC_LEGACY_URL: {process.env.NEXT_PUBLIC_LEGACY_URL}
+        <br />
+        </pre>
+        </>
+    )
+}
+
+export default Test;
