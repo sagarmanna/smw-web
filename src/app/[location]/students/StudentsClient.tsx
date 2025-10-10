70d5b91c59bb32898e5bbef1b70c779bfc67d0a5
manic56 <manikandan@pearlthoughts.com>
2025-10-10 13:02:22 +0530

    Add StudentsPage and StudentsClient components
    - Introduced StudentsPage component to handle student data based on location.
    - Created StudentsClient component for rendering student-related UI.
    - Updated PageAccessControl to include access checks for the new /students route.

---
+++ src/app/[location]/students/StudentsClient.tsx
@@ -0,0 +1,5 @@
+const StudentsClient = ({ location }: { location: Location }  ) => {
+    return <div>StudentsClient </div>;
+};
+
+export default StudentsClient;
---
+++ src/app/[location]/students/page.tsx
@@ -0,0 +1,14 @@
+import StudentsClient from "./StudentsClient";
+
+interface StudentsPageProps {
+  params: Promise<{
+    location: Location;
+  }>;
+}
+
+export default async function StudentsPage({ params }: StudentsPageProps) {
+  const { location } = await params;
+  return <StudentsClient location={location} />;
+}
+
+
