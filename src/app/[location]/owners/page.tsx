6fa6b480aa4c899ad0b2cbf01b7b5549ce04cfb2
manic56 <manikandan@pearlthoughts.com>
2025-12-31 11:17:42 +0530

    feat: add Owners and Staff Members pages with access control updates
    - Introduced OwnersPage and StaffMembersPage components for dedicated views.
    - Updated PageAccessControl to include new paths for owners and staff members, enhancing navigation and access management.

---
+++ src/app/[location]/owners/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OwnersPage = () => {
+  return (
+    <div>
+      <h1>Owners</h1>
+    </div>
+  )
+}
+
+export default OwnersPage
---
+++ src/app/[location]/staff-members/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StaffMembersPage = () => {
+  return (
+    <div>
+      <h1>Staff Members</h1>
+    </div>
+  )
+}
+
+export default StaffMembersPage
